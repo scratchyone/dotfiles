00d4b61b7e9b6943cc8cad95327cd2d9a4fe930d
scratchyone <scratchywon@gmail.com>
2021-03-01 09:21:19 -0500

    Fix missing directory

--- setup-debian.sh
+++ setup-debian.sh
@@ -3,4 +3,5 @@ echo "eval \"$(starship init bash)\"" >> ~/.bashrc
 sudo apt install neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+mkdir -p "~/.config/nvim/"
 cp init.vim "~/.config/nvim/"
