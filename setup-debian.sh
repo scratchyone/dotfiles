3f018696d5f204566a9cbc26d9053d121d47b597
scratchyone <scratchywon@gmail.com>
2021-03-01 09:16:31 -0500

    Added debian install script

---
+++ setup-debian.sh
@@ -0,0 +1,6 @@
+curl -fsSL https://starship.rs/install.sh | bash
+"eval \"$(starship init bash)\"" >> ~/.bashrc
+sudo apt install neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+cp init.vim "~/.config/nvim/"
