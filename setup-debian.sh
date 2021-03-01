9394b1f1e814f1459571aeb4b59899d95b30f95b
scratchyone <scratchywon@gmail.com>
2021-03-01 14:24:11 +0000

    Fix incorrect vim-plug location

--- setup-debian.sh
+++ setup-debian.sh
@@ -1,7 +1,7 @@
 curl -fsSL https://starship.rs/install.sh | bash
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 sudo apt install neovim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+sh -c 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p "~/.config/nvim/"
 cp init.vim "~/.config/nvim/"
