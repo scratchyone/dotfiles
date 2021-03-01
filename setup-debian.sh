bd91da1b302bad06c005326bba02da3498c25885
scratchyone <scratchywon@gmail.com>
2021-03-01 10:22:59 -0500

    Fix paths

--- setup-debian.sh
+++ setup-debian.sh
@@ -1,9 +1,11 @@
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
 curl -fsSL https://starship.rs/install.sh | bash
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 sudo apt install neovim
 sh -c 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p ~/.config/nvim/
-ln -s $(dirname "$0")init.vim ~/.config/nvim/init.vim
-ln -s $(dirname "$0")starship.toml ~/.config/starship.toml
+ln -s "$SCRIPTPATH/init.vim" ~/.config/nvim/
+ln -s "$SCRIPTPATH/starship.toml" ~/.config/
 nvim -c ':PlugInstall' -c ":qa!"
