f8c527d81a34843f0963e0c2382713cc3d8a475c
scratchyone <scratchywon@gmail.com>
2021-03-01 10:00:11 -0500

    Add starship config

--- setup-debian.sh
+++ setup-debian.sh
@@ -5,4 +5,5 @@ sh -c 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p ~/.config/nvim/
 cp init.vim ~/.config/nvim/
+cp starship.toml ~/.config/starship.toml
 nvim -c ':PlugInstall' -c ":qa!"
