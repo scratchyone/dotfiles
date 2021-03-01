eaedcfe6277a9968f1a9ec971ca677223df97f04
scratchyone <scratchywon@gmail.com>
2021-03-01 10:09:26 -0500

    Replace copy with symlink

--- setup-debian.sh
+++ setup-debian.sh
@@ -4,6 +4,6 @@ sudo apt install neovim
 sh -c 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p ~/.config/nvim/
-cp init.vim ~/.config/nvim/
-cp starship.toml ~/.config/starship.toml
+ln -s init.vim ~/.config/nvim/init.vim
+ln -s starship.toml ~/.config/starship.toml
 nvim -c ':PlugInstall' -c ":qa!"
