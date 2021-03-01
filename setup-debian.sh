e7f267f4abbfff687dcc9e844aa161e2be4f36bf
scratchyone <scratchywon@gmail.com>
2021-03-01 09:55:18 -0500

    Remove quotes

--- setup-debian.sh
+++ setup-debian.sh
@@ -3,6 +3,6 @@ echo 'eval "$(starship init bash)"' >> ~/.bashrc
 sudo apt install neovim
 sh -c 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-mkdir -p "~/.config/nvim/"
-cp init.vim "~/.config/nvim/"
+mkdir -p ~/.config/nvim/
+cp init.vim ~/.config/nvim/
 nvim -c ':PlugInstall' -c ":qa!"
