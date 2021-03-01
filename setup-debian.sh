46ab82710245ff518b40e6867a5f7ef4325378ab
scratchyone <scratchywon@gmail.com>
2021-03-01 09:30:03 -0500

    Add plugin autoinstall

--- setup-debian.sh
+++ setup-debian.sh
@@ -5,3 +5,4 @@ sh -c 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p "~/.config/nvim/"
 cp init.vim "~/.config/nvim/"
+nvim -c ':PlugInstall' -c ":qa!"
