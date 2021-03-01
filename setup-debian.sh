3d156fdb7e8f336db7e84158a4d81ddf4c504722
scratchyone <scratchywon@gmail.com>
2021-03-01 09:22:37 -0500

    Fix quotes

--- setup-debian.sh
+++ setup-debian.sh
@@ -1,5 +1,5 @@
 curl -fsSL https://starship.rs/install.sh | bash
-echo "eval \"$(starship init bash)\"" >> ~/.bashrc
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
 sudo apt install neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
