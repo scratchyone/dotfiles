4128490b796a1f6af890354a6a22ace938590233
scratchyone <scratchywon@gmail.com>
2021-04-15 09:19:41 -0400

    Add bat

--- setup-debian.sh
+++ setup-debian.sh
@@ -3,6 +3,7 @@ SCRIPTPATH=`dirname $SCRIPT`
 curl -fsSL https://starship.rs/install.sh | bash
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 sudo apt install neovim
+sudo apt install bat
 sh -c 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p ~/.config/nvim/
